1c67bde517c4d3ac717f7b5c85f42bf4c8f0ef25
Ignacio Hidalgo <i.hpower@alumnos.upm.es>
2023-12-20 20:06:07 +0100

    Pregunta 3

--- src/main/java/es/upm/grise/profundizacion/td3/ProductDelivery.java
+++ src/main/java/es/upm/grise/profundizacion/td3/ProductDelivery.java
@@ -12,13 +12,15 @@ public class ProductDelivery {
 	
 	private Vector<Order> orders = new Vector<Order>();
 	
-	public ProductDelivery() throws DatabaseProblemException {
+	private DriverManagerPersonal driverManager;
+	
+	public ProductDelivery(DriverManagerPersonal driverManager) throws DatabaseProblemException {
 		
 		// Orders are loaded into the orders vector for processing
 		try {
-			
+			this.driverManager = driverManager; 
 			// Create DB connection
-			Connection connection = DriverManager.getConnection("jdbc:sqlite:resources/orders.db");
+			Connection connection = this.driverManager.getConnection("jdbc:sqlite:resources/orders.db");
 
 			// Read from the orders table
 			String query = "SELECT * FROM orders";
@@ -48,34 +50,44 @@ public class ProductDelivery {
 	// Calculate the handling amount
 	public double calculateHandlingAmount() throws MissingOrdersException {
 		
+		//1
 		// This method can only be invoked when there are orders to process
 		if(orders.isEmpty())
+		//2
 			throw new MissingOrdersException();
 		
+		//3
 		// The handling amount is 2% of the orders' total amount
 		double handlingPercentage = SystemConfiguration.getInstance().getHandlingPercentage();
-		
+		//3
 		double totalAmount = 0;
+		//4
 		for(Order order : orders) {
+			//5
 			totalAmount += order.getAmount();				
 		}
 		
 		// However, it increases depending on the time of the day
 		// We need to know the hour of the day. Minutes and seconds are not relevant
+		//6
 		SimpleDateFormat sdf = new SimpleDateFormat("HH");	
 		Timestamp timestap = new Timestamp(System.currentTimeMillis());
 		int hour = Integer.valueOf(sdf.format(timestap));
 			
 		// and it also depends on the number of orders
+		//6
 		int numberOrders = orders.size();
 		
 		// When it is late and the number of orders is large
 		// the handling costs more
+		//7a 7b
 		if(hour >= 22 || numberOrders > 10) {
+			//8
 			handlingPercentage += 0.01;
 		}
 
 		// The final handling amount
+		//9
 		return totalAmount * handlingPercentage;
 		
 	}
--- src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
+++ src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
@@ -1,22 +1,34 @@
 package es.upm.grise.profundizacion.td3;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.sql.SQLException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 public class ProductDeliveryTest {
 	
 	ProductDelivery productDelivery;
+	DriverManagerPersonal driverManager;
 	
 	@BeforeEach
 	public void setUp() throws DatabaseProblemException {
-		productDelivery = new ProductDelivery();
+		driverManager = mock(DriverManagerPersonal.class);
 	}
 	
 	@Test
-	public void test() throws MissingOrdersException  {
-		assertEquals(20, productDelivery.calculateHandlingAmount());
+	public void testPregunta3() throws SQLException, DatabaseProblemException {
+		when(driverManager.getConnection("jdbc:sqlite:resources/orders.db")).thenThrow(new DatabaseProblemException());
+		
+		assertThrows(DatabaseProblemException.class, () -> new ProductDelivery(driverManager));
+		
 	}
 
 }
