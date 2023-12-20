a642b07b755699be8d5c31e422ee6bca064e86f9
aloncrack7 <alonmadrid@gmail.com>
2023-12-20 20:33:01 +0100

    add: camino2

--- src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
+++ src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
@@ -1,13 +1,14 @@
 package es.upm.grise.profundizacion.td3;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.sql.DriverManager;
-
+import java.text.SimpleDateFormat;
 import java.util.Vector;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -45,8 +46,18 @@ public class ProductDeliveryTest {
 			productDelivery.calculateHandlingAmount();});
 	}
 
-	// @Test
-	// public void testCamino2(){
-		
-	// }
+	@Test
+	public void testCamino2(){
+		SimpleDateFormat mockDataFormaInteger=mock(SimpleDateFormat.class);
+		when(mockDataFormaInteger.format(anyLong())).thenReturn("23");
+
+		try {
+			assertTrue(productDelivery.calculateHandlingAmount()==20.);
+		} catch (MissingOrdersException e) {
+			fail();
+			e.printStackTrace();
+		}
+	}
+
+	
 }
