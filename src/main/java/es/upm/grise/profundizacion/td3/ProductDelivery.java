78efd2e506ef294944663ba3dfb1558a8929f9bb
Javier Mateo <javier.mateo.segura@alumnos.upm.es>
2023-12-20 19:12:48 +0100

    Create the index for the graph

--- src/main/java/es/upm/grise/profundizacion/td3/ProductDelivery.java
+++ src/main/java/es/upm/grise/profundizacion/td3/ProductDelivery.java
@@ -9,14 +9,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 
 public class ProductDelivery {
-	
+
 	private Vector<Order> orders = new Vector<Order>();
-	
+
 	public ProductDelivery() throws DatabaseProblemException {
-		
+
 		// Orders are loaded into the orders vector for processing
 		try {
-			
+
 			// Create DB connection
 			Connection connection = DriverManager.getConnection("jdbc:sqlite:resources/orders.db");
 
@@ -27,58 +27,58 @@ public class ProductDelivery {
 
 			// Iterate until we get all orders' data
 			while (resultSet.next()) {
-				
+
 				int id = resultSet.getInt("id");
 				double amount = resultSet.getDouble("amount");
 				orders.add(new Order(id, amount));
-				
+
 			}
 
 			// Close the connection
 			connection.close();
 
 		} catch (Exception e) {
-			
-			throw new DatabaseProblemException(); 
-			
+
+			throw new DatabaseProblemException();
+
 		}
 
 	}
 
 	// Calculate the handling amount
 	public double calculateHandlingAmount() throws MissingOrdersException {
-		
+
 		// This method can only be invoked when there are orders to process
-		if(orders.isEmpty())
-			throw new MissingOrdersException();
-		
+		if(orders.isEmpty()) // 1
+			throw new MissingOrdersException(); // 2
+
 		// The handling amount is 2% of the orders' total amount
-		double handlingPercentage = SystemConfiguration.getInstance().getHandlingPercentage();
-		
+		double handlingPercentage = SystemConfiguration.getInstance().getHandlingPercentage(); //3
+
 		double totalAmount = 0;
-		for(Order order : orders) {
-			totalAmount += order.getAmount();				
+		for(Order order : orders) { //4
+			totalAmount += order.getAmount(); //5
 		}
-		
+
 		// However, it increases depending on the time of the day
 		// We need to know the hour of the day. Minutes and seconds are not relevant
-		SimpleDateFormat sdf = new SimpleDateFormat("HH");	
+		SimpleDateFormat sdf = new SimpleDateFormat("HH"); //6
 		Timestamp timestap = new Timestamp(System.currentTimeMillis());
 		int hour = Integer.valueOf(sdf.format(timestap));
-			
+
 		// and it also depends on the number of orders
 		int numberOrders = orders.size();
-		
+
 		// When it is late and the number of orders is large
 		// the handling costs more
-		if(hour >= 22 || numberOrders > 10) {
-			handlingPercentage += 0.01;
+		if(hour >= 22 || numberOrders > 10) { //7
+			handlingPercentage += 0.01; //8
 		}
 
 		// The final handling amount
-		return totalAmount * handlingPercentage;
-		
+		return totalAmount * handlingPercentage; //9
+
 	}
 
-	
+
 }
