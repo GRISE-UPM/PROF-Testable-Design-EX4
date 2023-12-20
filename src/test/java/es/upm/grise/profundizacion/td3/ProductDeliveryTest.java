18dad6bac21f4a5683a90231bc9c8d862c316ea4
antoniocastajr <antoniocastajr@outlook.es>
2023-12-20 20:32:08 +0100

    Segundo test

--- src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
+++ src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
@@ -2,7 +2,9 @@ package es.upm.grise.profundizacion.td3;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.Statement;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -17,26 +19,50 @@ public class ProductDeliveryTest {
 	
 	@BeforeEach
 	public void setUp() throws DatabaseProblemException {
-		// productDelivery = new ProductDelivery();
+		//productDelivery = new ProductDelivery();
 	}
 
 	@Test 
 	public void test1() throws DatabaseProblemException, SQLException{
 		Connection mockConnection = Mockito.mock(Connection.class);
 
-		// Simulamos un error de base de datos
+		// Comportamiento del mock
 		Mockito.when(mockConnection.createStatement()).thenThrow(new SQLException());
 			
 		// Comprobaciones
 		assertThrows(DatabaseProblemException.class, () -> new ProductDelivery(mockConnection));
 		verify(mockConnection,times(1)).createStatement();
 
-}
-
-	
-	@Test
-	public void test() throws MissingOrdersException  {
-		// assertEquals(20, productDelivery.calculateHandlingAmount());
 	}
+
+ 
+	@Test
+	// Primer camino: Se lanza la excepción MissingOrdersThrowsException()
+	public void testC1() throws Exception {
+		// Crear un mock de la conexión
+		Connection mockConnection = Mockito.mock(Connection.class);
+		Statement mockStatement = Mockito.mock(Statement.class);
+		ResultSet mockResultSet = Mockito.mock(ResultSet.class);
+	
+		// Configurar los mocks para simular el comportamiento deseado
+		Mockito.when(mockConnection.createStatement()).thenReturn(mockStatement);
+		Mockito.when(mockStatement.executeQuery(Mockito.anyString())).thenReturn(mockResultSet);
+		Mockito.when(mockResultSet.next()).thenReturn(false);
+	
+		// Crear la instancia de ProductDelivery con la conexión mockeada
+		ProductDelivery productDelivery = new ProductDelivery(mockConnection);
+	
+		// Verificar que se lanza MissingOrdersException
+		assertThrows(MissingOrdersException.class, () -> productDelivery.calculateHandlingAmount());
+	}
+
+	@Test
+	// Segundo camino: 
+	public void testC2() {
+
+
+	}
+
+
 
 }
