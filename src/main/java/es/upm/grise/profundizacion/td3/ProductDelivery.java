1d4ca4a56386cfc3904359133501d69d74292665
190236 <andres.garcia.solorzano@alumnos.upm.es>
2023-12-20 19:33:10 +0100

    añadidos los comentarios para la pregunta 1

--- src/main/java/es/upm/grise/profundizacion/td3/ProductDelivery.java
+++ src/main/java/es/upm/grise/profundizacion/td3/ProductDelivery.java
@@ -46,39 +46,39 @@ public class ProductDelivery {
 	}
 
 	// Calculate the handling amount
-	public double calculateHandlingAmount() throws MissingOrdersException {
-		
+	public double calculateHandlingAmount() throws MissingOrdersException {                           //1
+
 		// This method can only be invoked when there are orders to process
-		if(orders.isEmpty())
-			throw new MissingOrdersException();
-		
+		if(orders.isEmpty())																		  //2
+			throw new MissingOrdersException();												          //3
+
 		// The handling amount is 2% of the orders' total amount
-		double handlingPercentage = SystemConfiguration.getInstance().getHandlingPercentage();
-		
-		double totalAmount = 0;
-		for(Order order : orders) {
-			totalAmount += order.getAmount();				
+		double handlingPercentage = SystemConfiguration.getInstance().getHandlingPercentage();        //4
+
+		double totalAmount = 0;																          //4
+		for(Order order : orders) {																      //4
+			totalAmount += order.getAmount();														  //5
 		}
-		
+
 		// However, it increases depending on the time of the day
 		// We need to know the hour of the day. Minutes and seconds are not relevant
-		SimpleDateFormat sdf = new SimpleDateFormat("HH");	
-		Timestamp timestap = new Timestamp(System.currentTimeMillis());
-		int hour = Integer.valueOf(sdf.format(timestap));
-			
+		SimpleDateFormat sdf = new SimpleDateFormat("HH");									  //6
+		Timestamp timestap = new Timestamp(System.currentTimeMillis());						          //6
+		int hour = Integer.valueOf(sdf.format(timestap));								              //6
+
 		// and it also depends on the number of orders
-		int numberOrders = orders.size();
-		
+		int numberOrders = orders.size();													          //6
+
 		// When it is late and the number of orders is large
 		// the handling costs more
-		if(hour >= 22 || numberOrders > 10) {
-			handlingPercentage += 0.01;
+		if(hour >= 22 || numberOrders > 10) {													    //6,7
+			handlingPercentage += 0.01;														        //8
 		}
 
 		// The final handling amount
-		return totalAmount * handlingPercentage;
-		
+		return totalAmount * handlingPercentage;												    //9
+
 	}
 
-	
+
 }
