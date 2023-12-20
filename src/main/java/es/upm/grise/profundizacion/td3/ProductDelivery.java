3645c276d78a99dd73ee3f6222e3c0721dea5445
poll2000 <105507228+poll2000@users.noreply.github.com>
2023-12-20 19:13:43 +0100

    Añadir comentarios con los nodos

--- src/main/java/es/upm/grise/profundizacion/td3/ProductDelivery.java
+++ src/main/java/es/upm/grise/profundizacion/td3/ProductDelivery.java
@@ -46,23 +46,23 @@ public class ProductDelivery {
 	}
 
 	// Calculate the handling amount
-	public double calculateHandlingAmount() throws MissingOrdersException {
+	public double calculateHandlingAmount() throws MissingOrdersException { //NODO 1
 		
 		// This method can only be invoked when there are orders to process
-		if(orders.isEmpty())
-			throw new MissingOrdersException();
+		if(orders.isEmpty()) //NODO 2
+			throw new MissingOrdersException(); //NODO 3
 		
 		// The handling amount is 2% of the orders' total amount
-		double handlingPercentage = SystemConfiguration.getInstance().getHandlingPercentage();
+		double handlingPercentage = SystemConfiguration.getInstance().getHandlingPercentage();//NODO 4
 		
 		double totalAmount = 0;
-		for(Order order : orders) {
-			totalAmount += order.getAmount();				
+		for(Order order : orders) {//NODO 4
+			totalAmount += order.getAmount();	//NODO 5			
 		}
 		
 		// However, it increases depending on the time of the day
 		// We need to know the hour of the day. Minutes and seconds are not relevant
-		SimpleDateFormat sdf = new SimpleDateFormat("HH");	
+		SimpleDateFormat sdf = new SimpleDateFormat("HH");				//NODO 6
 		Timestamp timestap = new Timestamp(System.currentTimeMillis());
 		int hour = Integer.valueOf(sdf.format(timestap));
 			
@@ -71,12 +71,12 @@ public class ProductDelivery {
 		
 		// When it is late and the number of orders is large
 		// the handling costs more
-		if(hour >= 22 || numberOrders > 10) {
-			handlingPercentage += 0.01;
+		if(hour >= 22 /*NODO 6a*/|| numberOrders > 10/*NODO 6b*/) {
+			handlingPercentage += 0.01; //NODO 7
 		}
 
 		// The final handling amount
-		return totalAmount * handlingPercentage;
+		return totalAmount * handlingPercentage; //NODO 8
 		
 	}
 
