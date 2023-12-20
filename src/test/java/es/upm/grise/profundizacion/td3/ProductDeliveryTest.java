a3388af51c2aaebdc15a5a338a27092bb8f6b2c7
Eric García <114096503+Palid0@users.noreply.github.com>
2023-12-20 20:26:03 +0100

    Añadido primer camino del ejercicio 4

--- src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
+++ src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
@@ -1,22 +1,51 @@
 package es.upm.grise.profundizacion.td3;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
+import java.sql.SQLException;
+import java.util.Vector;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class ProductDeliveryTest {
 	
 	ProductDelivery productDelivery;
-	
-	@BeforeEach
-	public void setUp() throws DatabaseProblemException {
-		productDelivery = new ProductDelivery();
-	}
-	
+
+	//@Test
+	//public void testDatabaseError() throws DatabaseProblemException {
+	//	DatabaseAccessImpl mockDbAccess = Mockito.spy(DatabaseAccessImpl.class);
+	//
+    //    try {
+    //        Mockito.when(mockDbAccess.executeQuery(Mockito.anyString())).thenThrow(SQLException.class);
+	//
+    //        assertThrows(DatabaseProblemException.class, () -> {
+    //            new ProductDelivery(mockDbAccess);
+    //        });
+    //    } catch (SQLException e) {
+    //        // Handle the exception or fail the test if this exception is not expected
+    //        fail("Unexpected SQLException thrown in test");
+    //    }
+	//}
+
 	@Test
-	public void test() throws MissingOrdersException  {
-		assertEquals(20, productDelivery.calculateHandlingAmount());
+	public void testFirstPath() throws MissingOrdersException {
+		Vector<Order> mockedOrders = spy (new Vector<Order>());
+
+		try {
+			productDelivery = new ProductDelivery();
+		} catch (DatabaseProblemException e) {
+			fail("Unexpected DatabaseProblemException");
+		}
+
+		doReturn(true).when(mockedOrders).isEmpty();
+		productDelivery.orders = mockedOrders;
+		assertThrows(MissingOrdersException.class, () -> {
+                productDelivery.calculateHandlingAmount();
+            });
 	}
 
 }
