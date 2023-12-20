66e7fe525c7a31373d2f6230088a238622cc74c1
aloncrack7 <alonmadrid@gmail.com>
2023-12-20 20:20:44 +0100

    Camino 1

--- src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
+++ src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
@@ -1,6 +1,14 @@
 package es.upm.grise.profundizacion.td3;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.sql.DriverManager;
+
+import java.util.Vector;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,4 +27,26 @@ public class ProductDeliveryTest {
 		assertEquals(20, productDelivery.calculateHandlingAmount());
 	}
 
+	// @Test
+	// public void testDataBaseProblemException() {
+	// 	DriverManager mockMannager=mock(DriverManager.class);
+		
+		
+	// 	assertThrows(DatabaseProblemException.class, () -> {
+	// 		new ProductDelivery();
+	// 	});
+	// }
+
+	@Test
+	public void testCamino1(){
+		productDelivery.orders.clear();
+
+		assertThrows(MissingOrdersException.class, ()->{
+			productDelivery.calculateHandlingAmount();});
+	}
+
+	// @Test
+	// public void testCamino2(){
+		
+	// }
 }
