c9379847fac83d66c702f62f22188599927774e4
antoniocastajr <antoniocastajr@outlook.es>
2023-12-20 19:51:43 +0100

    Primer test

--- src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
+++ src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
@@ -2,8 +2,14 @@ package es.upm.grise.profundizacion.td3;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class ProductDeliveryTest {
 	
@@ -11,12 +17,26 @@ public class ProductDeliveryTest {
 	
 	@BeforeEach
 	public void setUp() throws DatabaseProblemException {
-		productDelivery = new ProductDelivery();
+		// productDelivery = new ProductDelivery();
 	}
+
+	@Test 
+	public void test1() throws DatabaseProblemException, SQLException{
+		Connection mockConnection = Mockito.mock(Connection.class);
+
+		// Simulamos un error de base de datos
+		Mockito.when(mockConnection.createStatement()).thenThrow(new SQLException());
+			
+		// Comprobaciones
+		assertThrows(DatabaseProblemException.class, () -> new ProductDelivery(mockConnection));
+		verify(mockConnection,times(1)).createStatement();
+
+}
+
 	
 	@Test
 	public void test() throws MissingOrdersException  {
-		assertEquals(20, productDelivery.calculateHandlingAmount());
+		// assertEquals(20, productDelivery.calculateHandlingAmount());
 	}
 
 }
