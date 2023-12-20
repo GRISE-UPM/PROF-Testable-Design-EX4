74f0a27e2d4155e8c4768509a8eb65486749d8f1
Samuel Dámaso <sdamasodemiguel@gmail.com>
2023-12-20 20:13:04 +0100

    TEST DATABASE EXCEPTION

--- src/main/java/es/upm/grise/profundizacion/td3/ProductDelivery.java
+++ src/main/java/es/upm/grise/profundizacion/td3/ProductDelivery.java
@@ -5,20 +5,18 @@ import java.sql.Statement;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 
 public class ProductDelivery {
-	
+
 	private Vector<Order> orders = new Vector<Order>();
-	
-	public ProductDelivery() throws DatabaseProblemException {
-		
+
+	public ProductDelivery(CustomDriverManager driver) throws DatabaseProblemException {
 		// Orders are loaded into the orders vector for processing
 		try {
-			
+
 			// Create DB connection
-			Connection connection = DriverManager.getConnection("jdbc:sqlite:resources/orders.db");
+			Connection connection = driver.getConnection("jdbc:sqlite:resources/orders.db");
 
 			// Read from the orders table
 			String query = "SELECT * FROM orders";
@@ -27,58 +25,57 @@ public class ProductDelivery {
 
 			// Iterate until we get all orders' data
 			while (resultSet.next()) {
-				
+
 				int id = resultSet.getInt("id");
 				double amount = resultSet.getDouble("amount");
 				orders.add(new Order(id, amount));
-				
+
 			}
 
 			// Close the connection
 			connection.close();
 
 		} catch (Exception e) {
-			
-			throw new DatabaseProblemException(); 
-			
+
+			throw new DatabaseProblemException();
+
 		}
 
 	}
 
 	// Calculate the handling amount
-	public double calculateHandlingAmount() throws MissingOrdersException { //NODO 1
-		
+	public double calculateHandlingAmount() throws MissingOrdersException { // NODO 1
+
 		// This method can only be invoked when there are orders to process
-		if(orders.isEmpty()) //NODO 2
-			throw new MissingOrdersException(); //NODO 3
-		
+		if (orders.isEmpty()) // NODO 2
+			throw new MissingOrdersException(); // NODO 3
+
 		// The handling amount is 2% of the orders' total amount
 		double handlingPercentage = SystemConfiguration.getInstance().getHandlingPercentage();
-		
+
 		double totalAmount = 0;
-		for(Order order : orders) { //NODO 4
-			totalAmount += order.getAmount(); //NODO 5
+		for (Order order : orders) { // NODO 4
+			totalAmount += order.getAmount(); // NODO 5
 		}
-		
+
 		// However, it increases depending on the time of the day
 		// We need to know the hour of the day. Minutes and seconds are not relevant
-		SimpleDateFormat sdf = new SimpleDateFormat("HH");	
+		SimpleDateFormat sdf = new SimpleDateFormat("HH");
 		Timestamp timestap = new Timestamp(System.currentTimeMillis());
 		int hour = Integer.valueOf(sdf.format(timestap));
-			
+
 		// and it also depends on the number of orders
 		int numberOrders = orders.size();
-		
+
 		// When it is late and the number of orders is large
 		// the handling costs more
-		if(hour >= 22 /*NODO 6 */ || numberOrders > 10 /*NODO 8 */) {
-			handlingPercentage += 0.01;  //NODO 7
+		if (hour >= 22 /* NODO 6 */ || numberOrders > 10 /* NODO 8 */) {
+			handlingPercentage += 0.01; // NODO 7
 		}
 
 		// The final handling amount
-		return totalAmount * handlingPercentage; //NODO 9
-		
-	} //NODO SALIDA
+		return totalAmount * handlingPercentage; // NODO 9
 
-	
+	} // NODO SALIDA
+
 }
--- src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
+++ src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
@@ -1,22 +1,41 @@
 package es.upm.grise.profundizacion.td3;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 public class ProductDeliveryTest {
-	
+
 	ProductDelivery productDelivery;
-	
+	CustomDriverManager driver;
+
 	@BeforeEach
 	public void setUp() throws DatabaseProblemException {
-		productDelivery = new ProductDelivery();
+		driver = mock(CustomDriverManager.class);
 	}
-	
+
 	@Test
-	public void test() throws MissingOrdersException  {
+	public void test() throws MissingOrdersException {
 		assertEquals(20, productDelivery.calculateHandlingAmount());
+	}
+
+	@Test
+	public void dataBaseProblemException() throws DatabaseProblemException, SQLException {
+		when(driver.getConnection(anyString())).thenThrow(new DatabaseProblemException());
+
+		assertThrows(DatabaseProblemException.class, () -> {
+			new ProductDelivery(driver);
+		});
 	}
 
 }
