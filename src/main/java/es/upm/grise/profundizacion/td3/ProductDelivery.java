ff00dc40ba7cb88d6ac90435c38332bb6b90f492
CarlosD <113545320+CarlosD21@users.noreply.github.com>
2023-12-20 19:14:28 +0100

    pregunta 1
    Comentarios con los nodos

--- src/main/java/es/upm/grise/profundizacion/td3/ProductDelivery.java
+++ src/main/java/es/upm/grise/profundizacion/td3/ProductDelivery.java
@@ -46,37 +46,38 @@ public class ProductDelivery {
 	}
 
 	// Calculate the handling amount
-	public double calculateHandlingAmount() throws MissingOrdersException {
+	public double calculateHandlingAmount() throws MissingOrdersException {//nodo 1
 		
 		// This method can only be invoked when there are orders to process
-		if(orders.isEmpty())
-			throw new MissingOrdersException();
+		if(orders.isEmpty())//nodo 2
+			throw new MissingOrdersException();//nodo 3
 		
 		// The handling amount is 2% of the orders' total amount
-		double handlingPercentage = SystemConfiguration.getInstance().getHandlingPercentage();
+		double handlingPercentage = SystemConfiguration.getInstance().getHandlingPercentage();//nodo 4
 		
-		double totalAmount = 0;
-		for(Order order : orders) {
-			totalAmount += order.getAmount();				
+		double totalAmount = 0;                                                               //nodo 4
+		for(Order order : orders) {//nodo 5
+			totalAmount += order.getAmount();// nodo 6				
 		}
 		
 		// However, it increases depending on the time of the day
 		// We need to know the hour of the day. Minutes and seconds are not relevant
-		SimpleDateFormat sdf = new SimpleDateFormat("HH");	
+		SimpleDateFormat sdf = new SimpleDateFormat("HH");//nodo 7	
 		Timestamp timestap = new Timestamp(System.currentTimeMillis());
 		int hour = Integer.valueOf(sdf.format(timestap));
 			
 		// and it also depends on the number of orders
-		int numberOrders = orders.size();
+		int numberOrders = orders.size();//nodo 7
 		
 		// When it is late and the number of orders is large
 		// the handling costs more
-		if(hour >= 22 || numberOrders > 10) {
-			handlingPercentage += 0.01;
+		if(hour >= 22 // nodo 8a
+		|| numberOrders > 10) {//nodo 8b
+			handlingPercentage += 0.01;// nodo 9
 		}
 
 		// The final handling amount
-		return totalAmount * handlingPercentage;
+		return totalAmount * handlingPercentage;// nodo 10
 		
 	}
 
