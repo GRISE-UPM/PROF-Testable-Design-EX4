e0b58fdbbb98e31f041658336077922b7c935ad5
PPedreg <147310421+PPedreg@users.noreply.github.com>
2023-12-20 20:26:01 +0100

    Primer test del camino 1-2-3-9

--- src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
+++ src/test/java/es/upm/grise/profundizacion/td3/ProductDeliveryTest.java
@@ -1,6 +1,13 @@
 package es.upm.grise.profundizacion.td3;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.*;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Vector;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,6 +24,12 @@ public class ProductDeliveryTest {
 	@Test
 	public void test() throws MissingOrdersException  {
 		assertEquals(20, productDelivery.calculateHandlingAmount());
+	}
+
+	@Test
+	public void testFirstPath() throws MissingOrdersException{ // 1 - 2 - 3 - 9
+		productDelivery.orders.clear();
+		assertThrows(MissingOrdersException.class, () -> productDelivery.calculateHandlingAmount());
 	}
 
 }
